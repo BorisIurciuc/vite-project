dbab8aa1edb06006314a7fa2d8180ee267e122e6
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-04-21 19:34:28 +0200

    test

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -5,10 +5,10 @@ export default function NavBar(): JSX.Element {
   return (
     <nav className={style.navigation}>
         <ul className={style.navList}>
-            <li><NavLink to="/" className={style.navElement}>StartPage</NavLink></li>
+            <li><NavLink to="/" className={style.navElement}>Start Page</NavLink></li>
             <li><NavLink to="/wurzburg" className={style.navElement}>Wurzburg</NavLink></li>
-            <li><NavLink to="/bayernSalad" className={style.navElement}>BayernSalad</NavLink></li>
-            <li><NavLink to="/titleImage" className={style.navElement}>TitleImage</NavLink></li>
+            <li><NavLink to="/bayernSalad" className={style.navElement}>Bayern Salad</NavLink></li>
+            <li><NavLink to="/titleImage" className={style.navElement}>Title Image</NavLink></li>
             <li><NavLink to="/wines" className={style.navElement}>Wines</NavLink></li>
             <li><NavLink to="/menuApp" className={style.navElement}>Menu</NavLink></li>
         </ul>
